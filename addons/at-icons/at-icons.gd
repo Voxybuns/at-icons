f54075449634f0562e9da80282790a36014e5ecb
Voxybuns <42387308+Voxybuns@users.noreply.github.com>
2026-07-03 18:19:28 +0200

    add alternate in-editor picker as a dock
    this is an adaptation of @SuperDuperSword 's recreation of the web picker, styled and treated as an editor dock rather than a standalone scene to improve the usability and integrate it better in the editor.

--- addons/at-icons/at-icons.gd
+++ addons/at-icons/at-icons.gd
@@ -3,7 +3,7 @@ extends EditorPlugin
 
 const PATH := "res://addons/at-icons/at_icons_browser.scn"
 const MENU_ITEM := "Open @icons picker scene"
-
+var dock : EditorDock
 
 #func _enable_plugin() -> void:
 	## Add autoloads here.
@@ -18,11 +18,19 @@ const MENU_ITEM := "Open @icons picker scene"
 func _enter_tree() -> void:
 	# Initialization of the plugin goes here.
 	add_tool_menu_item(MENU_ITEM, open_picker)
-
+	dock = EditorDock.new()
+	dock.dock_icon = preload("res://addons/at-icons/node/at.svg")
+	dock.default_slot = EditorDock.DOCK_SLOT_RIGHT_UL
+	var dock_content := preload("res://addons/at-icons/icon_browser.tscn").instantiate()
+	dock.add_child(dock_content)
+	add_dock(dock)
 
 func _exit_tree() -> void:
 	# Clean-up of the plugin goes here.
 	remove_tool_menu_item(MENU_ITEM)
+	remove_dock(dock)
+	dock.queue_free()
+	dock = null
 
 
 func open_picker() -> void:
