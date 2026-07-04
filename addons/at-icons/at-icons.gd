a512473b1ba588fc439b7d80070bf7044901b5b3
Voxy <42387308+Voxybuns@users.noreply.github.com>
2026-07-05 00:39:09 +0200

    remove old scene, amend license and config author metadata

--- addons/at-icons/at-icons.gd
+++ addons/at-icons/at-icons.gd
@@ -1,15 +1,13 @@
 @tool
 extends EditorPlugin
 
-const PATH := "res://addons/at-icons/at_icons_browser.scn"
-const MENU_ITEM := "Open @icons picker scene"
 var dock : EditorDock
 
 #func _enable_plugin() -> void:
 	## Add autoloads here.
 	#pass
-#
-#
+
+
 #func _disable_plugin() -> void:
 	## Remove autoloads here.
 	#pass
@@ -17,7 +15,6 @@ var dock : EditorDock
 
 func _enter_tree() -> void:
 	# Initialization of the plugin goes here.
-	add_tool_menu_item(MENU_ITEM, open_picker)
 	dock = EditorDock.new()
 	dock.dock_icon = preload("res://addons/at-icons/node/at.svg")
 	dock.default_slot = EditorDock.DOCK_SLOT_RIGHT_UL
@@ -27,11 +24,6 @@ func _enter_tree() -> void:
 
 func _exit_tree() -> void:
 	# Clean-up of the plugin goes here.
-	remove_tool_menu_item(MENU_ITEM)
 	remove_dock(dock)
 	dock.queue_free()
 	dock = null
-
-
-func open_picker() -> void:
-	EditorInterface.play_custom_scene(PATH)
